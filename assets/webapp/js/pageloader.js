cb27bf99069592e0d4cf22a1a6f0d490bde7f3c2
eye2web <remcovanderheijden@hotmail.com>
2014-05-08 14:10:49 +0200

    Intro is nu een overlay

--- assets/webapp/js/pageloader.js
+++ assets/webapp/js/pageloader.js
@@ -1,5 +1,3 @@
-$( document ).ready(function() {
- 
   // Function to load html pages 
   function LoadHtmlPage($page, $element)
   {					
@@ -9,10 +7,11 @@ $( document ).ready(function() {
   	});											
   }
 
+$( document ).ready(function() {
   // Call
   //LoadHtmlPage("test.html", $("#intro_video"));
 
-  LoadHtmlPage("pages/intro_video.html", $("#intro_video"));
+  //LoadHtmlPage("pages/intro_video.html", $("#intro_video"));
   LoadHtmlPage("pages/district.html", $("#blur"));
   
   LoadHtmlPage("pages/register.html", $("#register"));
