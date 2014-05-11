53cd4795ebfb197ce2a1e632faebd6f701227fb7
joey2210 <joey.dehaas1991@gmail.com>
2014-05-11 15:35:04 +0200

    moved js code to the html files,fixed postcode
    the move is temporarily to show the functionalities during the
    presentation

--- assets/webapp/js/Development/zoekpostcode.js
+++ assets/webapp/js/Development/zoekpostcode.js
@@ -1,27 +1,34 @@
  $(document).ready(function(){
-  $("#zoek").click(function(){
-      console.log("zoek clicked");
+      var aangevinkt;
+  $(document).on("click","#zoek",function(){
+     // console.log("zoek clicked");
       $("#results").empty();
+       aangevinkt= false;
       var zoekAdres= $("#zoekwijk").val();
     $.get("http://glas.mycel.nl/districts?search="+zoekAdres,function(data,status){
-      console.log("Data: " +JSON.stringify(data) + "\nStatus: " + status);
+     // console.log("Data: " +JSON.stringify(data) + "\nStatus: " + status);
       for (var i=0; i < data.length; i++){
            $( "#results" ).append( '<li><a data="'+data[i].id+'" class="linkwijk">'+data[i].name+'</a></li>' );  
       }
     });
   });
-   $("a.linkwijk").click(function(){
-      alert("linkwijk clicked");
-  // var id= $(this).attr('data');
+  
+   $(document).on("click","a.linkwijk",function(){
+        if(aangevinkt==true){
+            alert("je hebt al een wijk gekozen");
+        }else{
+       aangevinkt= true;
+      //alert("linkwijk clicked");
    $(this).css("color","Red");
-   $('this').attr('gekozen', 'gekozen');
-   //$("p").css({"background-color":"yellow","font-size":"200%"});
-   //console.log(id);
+   $(this).attr('gekozen', 'gekozen');
+   console.log(id);
+   }
   });
   
-  $("#ditismijnwijk").click(function(){
+  
+  $(document).on("click","#ditismijnwijk",function(){
      var gekozen= $("a.linkwijk[gekozen]").attr("data");
      //uit data het id van het gekozen element gehaald
-     alert(gekozen);
+     alert("uw gekozen wijk is: "+gekozen);
   });
 });
