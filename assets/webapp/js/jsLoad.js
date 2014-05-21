88ad973a20f477e03624c381275d4ecf0a24e2d0
joey2210 <joey.dehaas1991@gmail.com>
2014-05-21 15:00:56 +0200

    added maps

--- assets/webapp/js/jsLoad.js
+++ assets/webapp/js/jsLoad.js
@@ -3,7 +3,6 @@
 document.write('<script type="text/javascript" charset="utf-8" src="js/jquery/jquery-2.1.1.min.js"></script> ');
 document.write('<script type="text/javascript" charset="UTF-8" src="js/jquery/jquery-ui-1.10.4.custom.min.js"></script>');
 document.write('<script type="text/javascript" src="js/swiper/idangerous.swiper.min.js"></script>');
-//document.write('<script type="text/javascript" charset="UTF-8" src="js/vague.js"></script>');
 document.write('<script type="text/javascript" charset="UTF-8" src="js/blur.js"></script>');
 
 
