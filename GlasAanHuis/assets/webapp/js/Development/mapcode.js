3fdcc9d35c97782ee0403d7a7a44ef17021c43ef
joey2210 <joey.dehaas1991@gmail.com>
2014-05-30 18:54:27 +0200

    added the big map view

--- GlasAanHuis/assets/webapp/js/Development/mapcode.js
+++ GlasAanHuis/assets/webapp/js/Development/mapcode.js
@@ -1,10 +1,11 @@
       $( document ).ready(function() {
             //$(function() {
                 var map = L.mapbox.map('map', 'nanne.i84f0he3');
-                $.get('http://glas.mycel.nl/geo', function(data){
-                    map.setView(data.centroid.reverse(), 14);
-                    for (var i = 0; i < data.wijken.length; i++) {
-                        var wijk = data.wijken[i];var p = wijk.percentage;
+                 $.get('http://glas.mycel.nl/geo?id=2', function(data){
+                //console.log(JSON.stringify(data));
+                map.setView(data.center.reverse(), 14);
+                    for (var i = 0; i < data.bound; i++) {
+                        var wijk = data.bound[i];var p = wijk.percentage;
                         var red = Math.ceil((100 - Math.max(50, p)) * (255/50));
                         var green = Math.ceil(Math.min(50, p) * (255/50));
 
