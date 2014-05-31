912749a707287686e3bbb063748e007c0952a445
joey2210 <joey.dehaas1991@gmail.com>
2014-05-31 15:28:26 +0200

    Polygons are drawn again!

--- GlasAanHuis/assets/webapp/js/Development/mapcode.js
+++ GlasAanHuis/assets/webapp/js/Development/mapcode.js
@@ -1,27 +1,22 @@
  $( document ).ready(function() {
-            //$(function() {
                 var map = L.mapbox.map('map', 'nanne.i84f0he3');
                  $.get('http://glas.mycel.nl/geo?id=2', function(data){
-                console.log(JSON.stringify(data));
+               // console.log(JSON.stringify(data));
                 map.setView(data.center.reverse(), 14);
                 var wijkarray=[];
                 wijkarray=data.bound;
                     for (var i = 0; i < data.bound.length; i++) {
                        var wijk = data.bound[i];
-                       var p = wijk.percentage;                       
+                       var p = data.percentage;              
                         var red = Math.ceil((100 - Math.max(50, p)) * (255/50));
                         var green = Math.ceil(Math.min(50, p) * (255/50));
-                        console.log("wijk reverse: "+wijk.reverse());
-                        L.marker(wijk).addTo(map)
-                            .bindPopup(wijk.code + "")
-                            .openPopup();
-                        
-                       // var polygon = L.polygon(_.map(data.bound, function(a) { return a.reverse(); })).addTo(map);
-                       // polygon.setStyle({color: "black",weight: 3,fillColor: "rgb("+red+","+green+",0)",fillOpacity: 0.7});
+                        wijk.reverse();
                     }
-                    console.log("wijkarray: "+wijkarray);
+                    L.marker(data.center).addTo(map)
+                            .bindPopup("Uw wijk")
+                            .openPopup();
                      var polygon = L.polygon(wijkarray).addTo(map);
-         console.log(JSON.stringify(wijkarray));
-                        polygon.setStyle({color: "black",weight: 3,fillColor: "rgb("+red+","+green+",0)",fillOpacity: 0.7});
+                            //console.log(JSON.stringify(wijkarray));
+                     polygon.setStyle({color: "black",weight: 3,fillColor: "rgb("+red+","+green+",0)",fillOpacity: 0.7});
                 });
             });
